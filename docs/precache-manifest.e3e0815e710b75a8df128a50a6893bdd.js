f258d9cc59f1f65dd74de0d72a05ee629be565f2
Viktor Luft <viktor.luft@freiheit.com>
2019-12-09 08:26:38 +0100

    Added docs

---
+++ docs/precache-manifest.e3e0815e710b75a8df128a50a6893bdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74d419f36d51d04059a084972248c9d",
+    "url": "/cra-test/index.html"
+  },
+  {
+    "revision": "6672a1f6536d0b8f1cf6",
+    "url": "/cra-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a66e7fe6e86842a524f5",
+    "url": "/cra-test/static/js/2.13b00688.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cra-test/static/js/2.13b00688.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6672a1f6536d0b8f1cf6",
+    "url": "/cra-test/static/js/main.67979f3a.chunk.js"
+  },
+  {
+    "revision": "294049cd80f800eb850b",
+    "url": "/cra-test/static/js/runtime-main.37233c7d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cra-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
